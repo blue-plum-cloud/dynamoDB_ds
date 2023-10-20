5494cb730d14b9f9ff8f7f7ad6fe48e87fe02a4a
blue-plum-cloud <llester.leong@gmail.com>
2023-10-20 18:22:01 +0800

    add boilerplate files

---
+++ base/go.mod
@@ -0,0 +1,3 @@
+module github.com/blue-plum-cloud/dynamoDB_ds
+
+go 1.20
---
+++ base/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Starting the application...")
+	// Your application logic here
+}
