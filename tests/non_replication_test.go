0beff0b1d81492711a9b326bc413d19c8e31adc3
blue-plum-cloud <llester.leong@gmail.com>
2023-10-30 16:30:59 +0800

    slightly modified test

--- tests/non_replication_test.go
+++ tests/non_replication_test.go
@@ -14,23 +14,23 @@ func TestNoUpdateGetPut(t *testing.T) {
 	keyValuePairs := generateRandomKeyValuePairs(20, 100, 1000)
 
 	var tests = []struct {
-		numNodes, numTokens int
+		numNodes, numTokens, N, W int
 	}{
-		{5, 5},
-		{10, 10},
-		{40, 40},
-		{100, 100},
-		{800, 800},
+		{5, 5, 0, 0},
+		{10, 10, 0, 0},
+		{40, 40, 0, 0},
+		{100, 100, 0, 0},
+		{800, 800, 0, 0},
 
-		{5, 10},
-		{5, 12},
-		{15, 10},
-		{273, 927},
+		{5, 10, 0, 0},
+		{5, 12, 0, 0},
+		{15, 10, 0, 0},
+		{273, 927, 0, 0},
 	}
 	for _, tt := range tests {
 		testname := fmt.Sprintf("%d_nodes_%d_tokens", tt.numNodes, tt.numTokens)
 		t.Run(testname, func(t *testing.T) {
-			sys_config := base.Config{NUM_NODES: tt.numNodes, NUM_TOKENS: tt.numTokens}
+			sys_config := base.Config{NUM_NODES: tt.numNodes, NUM_TOKENS: tt.numTokens, N: tt.N, W: tt.W}
 			phy_nodes, close_ch := setUpNodes(&sys_config)
 
 			putKeyValuePairs(1, keyValuePairs, phy_nodes, &sys_config)
@@ -57,21 +57,22 @@ func TestMultipleGetPut(t *testing.T) {
 	keyValuePairs := generateRandomKeyValuePairs(200, 1000, 1000)
 
 	var tests = []struct {
-		numNodes, numTokens int
+		numNodes, numTokens, N, W int
 	}{
-		{5, 5},
-		{10, 10},
-		{40, 40},
-		{100, 100},
+		{5, 5, 0, 0},
+		{10, 10, 0, 0},
+		{40, 40, 0, 0},
+		{100, 100, 0, 0},
+		{800, 800, 0, 0},
 
-		{5, 10},
-		{5, 12},
-		{15, 10},
+		{5, 10, 0, 0},
+		{5, 12, 0, 0},
+		{15, 10, 0, 0},
 	}
 	for _, tt := range tests {
 		testname := fmt.Sprintf("%d_nodes_%d_tokens", tt.numNodes, tt.numTokens)
 		t.Run(testname, func(t *testing.T) {
-			sys_config := base.Config{NUM_NODES: tt.numNodes, NUM_TOKENS: tt.numTokens}
+			sys_config := base.Config{NUM_NODES: tt.numNodes, NUM_TOKENS: tt.numTokens, N: tt.N, W: tt.W}
 			phy_nodes, close_ch := setUpNodes(&sys_config)
 
 			putKeyValuePairs(1, keyValuePairs, phy_nodes, &sys_config)
