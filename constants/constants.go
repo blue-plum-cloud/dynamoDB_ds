0550bf18c0dcfb835ec088455a579214dfcdd9c7
Ian Goh <ian@iangohy.com>
2023-10-31 01:38:08 +0800

    Add debug constants to constants file

--- constants/constants.go
+++ constants/constants.go
@@ -1,13 +1,18 @@
 package constants
 
 const (
-	REQ_READ      = 0
-	REQ_WRITE     = 1
-	ACK           = 2
-	REQ_KILL      = 3
-	SET_DATA      = 4
-	READ_DATA     = 5
-	READ_DATA_ACK = 6
-	BACK_DATA     = 7
-	REQ_REVIVE    = 8
+	NO_DEBUG      = 1
+	INFO          = 2
+	VERBOSE_FIXED = 3
+	VERY_VERBOSE  = 4
+
+	REQ_READ      = 100
+	REQ_WRITE     = 101
+	ACK           = 102
+	REQ_KILL      = 103
+	SET_DATA      = 104
+	READ_DATA     = 105
+	READ_DATA_ACK = 106
+	BACK_DATA     = 107
+	REQ_REVIVE    = 108
 )
